5da7bf6e7ae0e8f99a523092cce2838aa35a3d97
DON <don@DONs-Laptop-9.local>
2023-04-30 01:45:23 -0400

    minor changes

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,3 +1,3 @@
 
-export const mainURL = 'https://6qk24xip7j.execute-api.us-east-1.amazonaws.com/prod/api ';
+export const mainURL = 'https://6qk24xip7j.execute-api.us-east-1.amazonaws.com/prod/api';
 export const proxyURL = 'https://6qk24xip7j.execute-api.us-east-1.amazonaws.com/prod/api/proxy';
